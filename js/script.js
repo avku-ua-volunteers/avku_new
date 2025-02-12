7b7437a4faa448ffc1c5df1409a3653f5eef12f1
4245877 <alner4245877@gmail.com>
2025-02-12 20:01:51 +0200

    Merge changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Скрипт для управления мобильным меню
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+  
+    hamburger.addEventListener('click', function() {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
