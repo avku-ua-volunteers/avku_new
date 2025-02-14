7d24fd449677776f30ca78785df9b3b55d46b568
4245877 <alner4245877@gmail.com>
2025-02-14 22:02:42 +0200

    Merge changes

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
       loadLanguage(lang);
     });
   });
+
+  // Обработчик клика по гамбургеру
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
 });
 
 /**
