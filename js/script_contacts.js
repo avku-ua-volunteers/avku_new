53e13204283ec02252fc876a8ba5440f21be04b6
4245877 <alner4245877@gmai.com>
2025-07-22 09:47:23 +0300

    Описание изменений

--- js/script_contacts.js
+++ js/script_contacts.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
 
         // ❗️ Вставьте сюда URL, который вам предоставил Vercel
-        const VERCEL_URL = 'https://имя-вашего-проекта.vercel.app/api/server';
+       const VERCEL_URL = 'https://avku-6o1u.vercel.app/api/server';
 
         statusMessage.textContent = 'Отправка...';
         statusMessage.style.color = 'blue';
